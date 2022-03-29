3f41a76ded3d12d5492497712f776bfa4249c0ec
Thomas Bnt <thomasbnt@protonmail.com>
2022-03-30 01:13:23 +0200

    Hello sponsors !

---
+++ README.md
@@ -0,0 +1,5 @@
+# Sponsors
+[![Sponsor generator](https://github.com/thomasbnt/sponsors/actions/workflows/sponsor_gen.yml/badge.svg)](https://github.com/thomasbnt/sponsors/actions/workflows/sponsor_gen.yml)
+
+
+![Sponsors](./sponsors.svg)
---
+++ sponsor.config.ts
@@ -0,0 +1,33 @@
+import { defineConfig, presets } from 'sponsorkit'
+
+export default defineConfig({
+  // Providers configs
+  github: {
+    login: 'thomasbnt',
+  },
+
+  // Rendering configs
+  width: 800,
+  formats: ['json', 'svg', 'png'],
+  tiers: [
+    {
+      title: 'Backers',
+      preset: presets.base,
+    },
+    {
+      title: 'Sponsors',
+      monthlyDollars: 1,
+      preset: presets.medium,
+    },
+    {
+      title: 'Silver Sponsors',
+      monthlyDollars: 5,
+      preset: presets.large,
+    },
+    {
+      title: 'Gold Sponsors',
+      monthlyDollars: 10,
+      preset: presets.xl,
+    },
+  ],
+})
